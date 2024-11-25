cb84d4a9379d76a543fced9c7e43e846fd0b1329
Ablayk <kalievv02a@gmail.com>
2024-11-25 10:24:39 +0500

    feat: add form

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,35 @@
 </script>
 
 <template>
-  
+  <div class="container">
+    <div class="row"></div>
+
+    <div class="col">
+      <h1 class="text-center my-3">Учет товаров</h1>
+
+      <div class="mb-3">
+  <label for="name" class="form-label">Название</label>
+  <input type="text" class="form-control" id="name">
+  </div>
+
+  <div class="mb-3">
+  <label for="date" class="form-label">Дата</label>
+  <input type="date" class="form-control" id="date">
+  </div>
+
+  <div class="mb-3">
+  <label for="count" class="form-label">Количество</label>
+  <input type="number" class="form-control" id="count">
+  </div>
+
+  <div class="mb-3">
+  <label for="price" class="form-label">Цена</label>
+  <input type="number" class="form-control" id="price">
+  </div>
+  <button type="button" class="btn btn-success">Добавить</button>
+</div>
+
+</div>
 
 
 </template>
