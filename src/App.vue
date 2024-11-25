28c2cca7747a0b594f1d08032471d83646c99d88
Ablayk <kalievv02a@gmail.com>
2024-11-25 10:41:24 +0500

    feat: add card

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="container">
-    <div class="row"></div>
+    <div class="row">
 
     <div class="col">
       <h1 class="text-center my-3">Учет товаров</h1>
@@ -29,9 +29,28 @@
   <input type="number" class="form-control" id="price">
   </div>
   <button type="button" class="btn btn-success">Добавить</button>
-</div>
 
 </div>
 
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
+      <div class="card-body">
+        <h5 class="card-title">HP</h5>
+        <p class="card-text">$1000</p>
+        <p class="card-text">10</p>
+        <p class="card-text">25.11.2024</p>
+      </div>
+      <div class="card-footer text-end">
+        <button class="btn btn-outline-danger">Удалить</button>
+      </div>
+    </div>
+  </div>
 
+
+
+
+</div>
+</div>
+</div>
 </template>
